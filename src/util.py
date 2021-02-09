480005681a10ea4cb5f77bb95db9826f13b559b0
Jimmy Hedman <jimmy.hedman@gmail.com>
2021-02-09 20:44:02 +0100

    Add util.get_secret_cookerid.
    - Connects to Anova cooker and fetches secret and cooker_id.

---
+++ src/util.py
@@ -0,0 +1,59 @@
+"""Util module for anova"""
+
+import socket
+from typing import Tuple
+
+
+def _decode(line: bytes) -> Tuple[int, str]:
+    """Decode Anova messages"""
+    rot = 1
+    output = ""
+    length = line[1]
+    recived_checksum = line[-1]
+    calculated_checksum = 0
+    for p in line[2:-1]:
+        output += chr(p >> (rot) | ((p & (2 ** rot) - 1) << 8 - rot))
+        rot += 1
+        rot %= 7
+        calculated_checksum += p
+
+    if recived_checksum == (calculated_checksum & 255):
+        return (length, output.rstrip())
+
+    return (0, "")
+
+
+def get_secret_cookerid(ip: str) -> Tuple[str, str]:
+    """Get secret and cooker_id from anova."""
+
+    anova_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    anova_socket.connect((ip, 9988))
+    count = 0
+    cooker_id = None
+    secret = None
+    while (cooker_id is None) and (secret is None):
+        line = bytearray()
+        if count % 128:
+            anova_socket.send(b"h")
+
+        data = anova_socket.recv(1024)
+        for d in range(0, len(data) - 1):
+            if (data[d], data[d + 1]) == (22, 104):
+                length, decoded = _decode(line)
+                if length == 22:
+                    cooker_id = decoded
+                if length == 11:
+                    secret = decoded
+
+                line = bytearray()
+            else:
+                line.append(data[d])
+
+        length, decoded = _decode(line)
+        if length == 22:
+            cooker_id = decoded
+        if length == 11:
+            secret = decoded
+        count += 1
+
+    return (cooker_id, secret)
